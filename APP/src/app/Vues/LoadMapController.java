1d925c27dbac31506bc25cbf55e886546f59cfff
Labrak Yanis <yanis.labrak@alumni.univ-avignon.fr>
2021-05-16 20:32:46 +0200

    Update

---
+++ APP/src/app/Vues/LoadMapController.java
@@ -0,0 +1,22 @@
+package app.Vues;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoadMapController {
+    
+    @FXML
+    void loadFrance(ActionEvent event) {
+        System.out.println("loadFrance");
+    }
+
+    @FXML
+    void loadEiffelTower(ActionEvent event) {
+        System.out.println("loadEiffelTower");
+    }
+
+    @FXML
+    void loadFile(ActionEvent event) {
+        System.out.println("loadFile");
+    }
+}
